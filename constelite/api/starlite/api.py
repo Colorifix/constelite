eebd9e2a2349770a8bf98d48d7db11909f18eaaa
Mihails Delmans <mihails.delmans@colorifix.com>
2023-02-10 17:17:56 +0000

    Add index page

--- constelite/api/starlite/api.py
+++ constelite/api/starlite/api.py
@@ -1,6 +1,19 @@
+from pathlib import Path
+
 from typing import Literal, Optional
 
-from starlite import Provide, Starlite, OpenAPIConfig
+from pydantic import Field
+
+from starlite import (
+    Provide,
+    Starlite,
+    OpenAPIConfig,
+    Template,
+    TemplateConfig,
+    get
+)
+
+from starlite.contrib.jinja import JinjaTemplateEngine
 
 from constelite.api.api import ConsteliteAPI
 
@@ -14,19 +27,47 @@ ControllerType = Literal['protocol', 'getter', 'setter']
 
 
 class StarliteAPI(ConsteliteAPI):
-    app: Optional[Starlite]
+    app: Optional[Starlite] = Field(exclude=True)
+    index_template: Optional[str]
+    template_dir: Optional[str]
+
+    def __init__(self, index_template=None, template_dir=None, **kwargs):
+        super().__init__(**kwargs)
+        self.index_template = index_template
+        self.template_dir = template_dir
 
     def provide_api(self):
         """Provides instance of self to route handlers
         """
         return self
 
+    def generate_index_route(self, index_template):
+        def index() -> Template:
+            return Template(name=index_template)
+        return get(path="/")(index)
+
     def generate_app(self) -> Starlite:
+        route_handlers = [
+            protocol_controller(self),
+            StoreController
+        ]
+
+        template_config = None
+
+        if self.index_template is not None and self.template_dir is not None:
+            route_handlers.append(
+                self.generate_index_route(self.index_template)
+            )
+
+            breakpoint()
+
+            template_config = TemplateConfig(
+                directory=Path(self.template_dir),
+                engine=JinjaTemplateEngine
+            )
+
         self.app = Starlite(
-            route_handlers=[
-                protocol_controller(self),
-                StoreController
-            ],
+            route_handlers=route_handlers,
             exception_handlers={
             },
             # openapi_config=OpenAPIConfig(
@@ -34,7 +75,8 @@ class StarliteAPI(ConsteliteAPI):
             #     version=self.version
             # ),
             openapi_config=None,
-            dependencies={"api": Provide(self.provide_api)}
+            dependencies={"api": Provide(self.provide_api)},
+            template_config=template_config
         )
 
         return self.app
