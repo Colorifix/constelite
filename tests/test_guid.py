a9baac34a43b3aaa9bd1d67eac8480270ce77bce
Mihails Delmans <mihails.delmans@colorifix.com>
2025-01-24 11:16:01 +0000

    Add in-memory guid map and use it in tests

--- tests/test_guid.py
+++ tests/test_guid.py
@@ -1,8 +1,9 @@
 import unittest
 
-from colorifix_alpha.util import get_config
-from constelite.store import NeofluxStore
-from constelite.guid_map import NeoGUIDMap
+from uuid import uuid4
+
+from constelite.store.memory import MemoryStore
+from constelite.guid_map.memory import MemoryGUID
 from constelite.models import StateModel, ref
 
 
@@ -18,27 +19,12 @@ class GuidTestContext():
         if self.initialised:
             return
     
-        self.store = NeofluxStore(
-            uid="78be519a-fdec-4e4a-85a5-26364ccf52e4",
-            name="StarGate",
-            neo_config={
-                "url": get_config("stores", "neoflux_store", "neo_config", "url"),
-                "auth": get_config("stores", "neoflux_store", "neo_config", "auth"),
-            },
-            influx_config={
-                "url": get_config("stores", "neoflux_store", "influx_config", "url"),
-                "token": get_config("stores", "neoflux_store", "influx_config", "token"),
-                "org": get_config("stores", "neoflux_store", "influx_config", "org"),
-                "bucket": get_config("stores", "neoflux_store", "influx_config", "bucket"),
-            }
+        self.store = MemoryStore(
+            uid=uuid4(),
+            name="MemoryStore",
         )
 
-        self.guid_map = NeoGUIDMap(
-            config={
-                "url": get_config("guid_map", "neo", "url"),
-                "auth": get_config("guid_map", "neo", "auth"),
-            }
-        )
+        self.guid_map = MemoryGUID()
 
         self.store.set_guid_map(guid_map=self.guid_map)
 
